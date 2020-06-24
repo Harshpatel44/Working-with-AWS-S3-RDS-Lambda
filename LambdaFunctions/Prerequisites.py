d84d3570c087950c7f48510ae1ff0149665a8289
Harsh Patel <hr402607@dal.ca>
2020-06-24 13:41:13 -0300

    extractFeatures complete

--- LambdaFunctions/Prerequisites.py
+++
@@ -1,8 +0,0 @@
-import nltk
-def prerequisites():
-    nltk.download("punkt", download_dir=str("/tmp/punkt"))
-    nltk.download("stopwords", download_dir=str("/tmp/stopwords"))
-    nltk.download("averaged_perceptron_tagger", download_dir=str("/tmp/averaged_perceptron_tagger"))
-    nltk.data.path.append("/tmp/punkt")
-    nltk.data.path.append("/tmp/stopwords")
-    nltk.data.path.append("/tmp/averaged_perceptron_tagger")
